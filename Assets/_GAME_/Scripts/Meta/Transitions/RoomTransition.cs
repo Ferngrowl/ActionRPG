7d49eb340f6c8004de768fbc171f6afa272e6088
1714546 <harveysutton09@gmail.com>
2024-10-21 16:24:31 +0100

    TextMeshPro and Location Text
    Added feature of displaying area name briefly on screen when entering a new area (resultantly added textmeshpro packages)

---
+++ Assets/_GAME_/Scripts/Meta/Transitions/RoomTransition.cs
@@ -0,0 +1,54 @@
+using UnityEngine;
+using Cinemachine;
+using UnityEngine.UI;
+using System.Collections;
+using TMPro;
+
+public class RoomTransition : MonoBehaviour
+{
+    
+
+    //room transition variables
+    public Vector2 playerOffset;  // How much to move the player
+    private CinemachineVirtualCamera cam;
+    public PolygonCollider2D newConfiner;  // New collider for the room
+    private CinemachineConfiner camConfiner; 
+
+    //location text vairables
+    public bool needText; // dependant on whether or not the area being moved too is a different biome or location rather than a different room in the same place
+    public string locationName;
+    public GameObject text;
+    public TextMeshProUGUI LocationText;
+
+    void Start()
+    {
+        cam = FindObjectOfType<CinemachineVirtualCamera>();
+        camConfiner = cam.GetComponent<CinemachineConfiner>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            // Move the player
+            other.transform.position += (Vector3)playerOffset;
+
+            // Change the confiner collider to the new room's collider
+            camConfiner.m_BoundingShape2D = newConfiner;
+            camConfiner.InvalidatePathCache();  // Clears the cache for the new collider to take effect
+            if (needText)
+            {
+                StartCoroutine(locationNameCo());
+            }
+
+        }
+    }
+
+    private IEnumerator locationNameCo()
+    {
+        text.SetActive(true);
+        LocationText.text = locationName;
+        yield return new WaitForSeconds(4f);
+        text.SetActive(false);
+    }
+}
